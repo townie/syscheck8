ff517313b7cb9defd633b4c78e359ce537df1c71
Keith Webber <keith.e.webber@gmail.com>
2014-04-14 09:55:25 -0400

    init + gems

---
+++ spec/features/car_salesperson_records_a_car_Spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'Car salesperson records a car', %q{
+  As a car salesperson
+  I want to record a newly acquired car
+  So that I can list it in my lot
+} do
+
+
+  end
